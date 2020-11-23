c308da49574990e410228241c31e982c51d6556b
Andrei Storozhko <andrei_storozhko@epam.com>
2020-11-23 23:32:53 +0300

    feat: improvements added

--- product-service/src/handlers/catalogBatchProcess.ts
+++ product-service/src/handlers/catalogBatchProcess.ts
@@ -4,24 +4,16 @@ import connectToDataBase from '../utils/connectToDataBase';
 import * as ProductService from '../services/products';
 import { DataBase } from '../interfaces/dataBase';
 import util  from 'util';
+import { Product } from '../interfaces/product';
 
 const processMessage = async (record: SQSRecord) => {
   let db: DataBase;
-  const sns = new AWS.SNS();
   const sqs = new AWS.SQS();
   const formattedBody = JSON.parse(record.body);
   try {
     db = await connectToDataBase();
     const product = await ProductService.createProduct(db, formattedBody);
     console.log('Product successfully created: ', product);
-
-    await sns.publish({
-      Subject: 'Product successfully saved',
-      Message: JSON.stringify(product, null, 2),
-      MessageAttributes: { status: { DataType: 'String', StringValue: 'fulfilled' } },
-      TopicArn: process.env.SNS_ARN,
-    }).promise();
-    console.log('Send email for: ', product);
 
     await sqs.deleteMessage({
       QueueUrl: process.env.SQS_URL,
@@ -32,14 +24,6 @@ const processMessage = async (record: SQSRecord) => {
     return product;
   } catch (e) {
     console.error(e.message, e.stack);
-    await sns.publish({
-      Subject: 'Product not saved',
-      Message: JSON.stringify(formattedBody, null, 2),
-      MessageAttributes: { status: { DataType: 'String', StringValue: 'rejected' } },
-      TopicArn: process.env.SNS_ARN,
-    }).promise();
-    console.log('Send email for: ', formattedBody);
-
     throw e;
   } finally {
     await db.sequelize.close();
@@ -49,12 +33,36 @@ const processMessage = async (record: SQSRecord) => {
 export const catalogBatchProcess: SQSHandler = async (event: SQSEvent) => {
   console.log('Start invoking catalogBatchProcess function', util.inspect(event, { depth: 5 }));
 
+  const sns = new AWS.SNS();
   const messagePromises = event.Records.map(processMessage);
   const processedMessages = await Promise.allSettled(messagePromises);
+
+  const succeededMessages = processedMessages
+    .filter((promise) => promise.status === 'fulfilled')
+    .map((promise: PromiseFulfilledResult<Product>) => promise.value);
   const failedMessages = processedMessages
-    .filter((promise) => promise.status === 'rejected');
+    .filter((promise) => promise.status === 'rejected')
+    .map((promise: PromiseRejectedResult) => promise.reason);
+
+  if (succeededMessages.length) {
+    await sns.publish({
+      Subject: 'Product successfully saved',
+      Message: JSON.stringify(succeededMessages, null, 2),
+      MessageAttributes: { status: { DataType: 'String', StringValue: 'fulfilled' } },
+      TopicArn: process.env.SNS_ARN,
+    }).promise();
+    console.log('Send email for: ', succeededMessages);
+  }
 
   if (failedMessages.length) {
+    await sns.publish({
+      Subject: 'Product not saved',
+      Message: JSON.stringify(failedMessages, null, 2),
+      MessageAttributes: { status: { DataType: 'String', StringValue: 'rejected' } },
+      TopicArn: process.env.SNS_ARN,
+    }).promise();
+    console.log('Send email for: ', failedMessages);
+
     console.error('Partial batch failure');
   }
   return null;
